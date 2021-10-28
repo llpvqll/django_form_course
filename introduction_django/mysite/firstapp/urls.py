46abc07530cc241623997089f5ff12692791af89
llpvqll <llpvo2602ll@gmail.com>
2021-10-28 15:41:15 +0300

    update first task

--- introduction_django/mysite/firstapp/urls.py
+++ introduction_django/mysite/firstapp/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path(r'^$', views.hello_django, name='hello_django'),
+    url(r'^$', views.hello_django, name='hello_django'),
 ]
 
 
