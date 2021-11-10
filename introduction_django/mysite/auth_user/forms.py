4efb22a7bcc49edc0030d8760bd06d2d4026c06b
llpvqll <llpvo2602ll@gmail.com>
2021-11-10 19:11:46 +0200

    update forms file

--- introduction_django/mysite/auth_user/forms.py
+++ introduction_django/mysite/auth_user/forms.py
@@ -7,14 +7,16 @@ from django.contrib.auth.forms import UserCreationForm
 
 class UserCreateForm(UserCreationForm):
     email = forms.EmailField(required=True)
+    phone = forms.IntegerField()
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2")
+        fields = ("username", "email", "phone", "password1", "password2")
 
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
+        user.email = self.cleaned_data["phone"]
         if commit:
             user.save()
         return user
