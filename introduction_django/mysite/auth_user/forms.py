941c653577722b289944e7f8a442f45e53b32693
llpvqll <llpvo2602ll@gmail.com>
2021-11-11 14:16:53 +0200

    update forms file

--- introduction_django/mysite/auth_user/forms.py
+++ introduction_django/mysite/auth_user/forms.py
@@ -16,7 +16,7 @@ class UserCreateForm(UserCreationForm):
     def save(self, commit=True):
         user = super(UserCreateForm, self).save(commit=False)
         user.email = self.cleaned_data["email"]
-        user.email = self.cleaned_data["phone"]
+        user.phone = self.cleaned_data["phone"]
         if commit:
             user.save()
         return user
